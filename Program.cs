1f0dbce2b412cbff0cc5e813903265997043dbf1
Abdimajid <Abdimajid.mahamed1@gmail.com>
2024-11-02 19:54:34 +0100

    another try i guess

--- Program.cs
+++ Program.cs
@@ -2,4 +2,4 @@
 Console.WriteLine("Hello from visual studio!");
 Console.WriteLine("hello from the first pull request made, hihi");
 Console.WriteLine("testing workflow changing in program cs");
-Console.WriteLine();
+Console.WriteLine("gdgfdgfdgfdgfd");
